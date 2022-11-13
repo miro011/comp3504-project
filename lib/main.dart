233393025ef2d8c0b56b630a9a4cfd4a7ddb1081
tmtkachyk <tmtkachyk@gmail.com>
2022-11-13 15:10:32 -0700

    Shows marker at current location. Not fully tested

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
 
 void main() => runApp(const MyApp());
 
@@ -13,10 +16,29 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   late GoogleMapController mapController;
 
-  final LatLng _center = const LatLng(45.521563, -122.677433);
+  //final LatLng _center = const LatLng(51.0447, 114.0719);
 
   void _onMapCreated(GoogleMapController controller) {
     mapController = controller;
+  }
+
+  //get devices current location
+  LocationData? currentLocation;
+  void getCurrentLocation () {
+    Location location = Location();
+
+    location.getLocation().then(
+          (location) {
+            currentLocation = location;
+    },
+    );
+    print(currentLocation);
+  }
+
+  @override
+  void initState() {
+    getCurrentLocation();
+    super.initState();
   }
 
   @override
@@ -27,13 +49,21 @@ class _MyAppState extends State<MyApp> {
           title: const Text('Maps Sample App'),
           backgroundColor: Colors.green[700],
         ),
-        body: GoogleMap(
-          onMapCreated: _onMapCreated,
-          initialCameraPosition: CameraPosition(
-            target: _center,
-            zoom: 11.0,
+        body: currentLocation == null ? const Text("Loading")
+            : GoogleMap(
+              onMapCreated: _onMapCreated,
+              initialCameraPosition: CameraPosition(
+                target: LatLng(currentLocation!.latitude!, currentLocation!.longitude!),
+                zoom: 11.0,
+              ),
+            markers: {
+                Marker(
+                  markerId: const MarkerId("currentLocation"),
+                  position: LatLng(currentLocation!.latitude!, currentLocation!.longitude!),
+                ),
+            },
+
           ),
-        ),
       ),
     );
   }
