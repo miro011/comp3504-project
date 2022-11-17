0196b01aeb5609d3503e52fa87b82718705f9a9e
tmtkachyk <tmtkachyk@gmail.com>
2022-11-17 11:29:13 -0700

    Get current location functional using geolocator

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'dart:ffi';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-//import 'package:location/location.dart';
+
 
 void main() => runApp(const MyApp());
 
@@ -17,23 +17,13 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   late GoogleMapController mapController;
 
-  //final LatLng _center = const LatLng(51.0447, 114.0719);
+
 
   void _onMapCreated(GoogleMapController controller) {
     mapController = controller;
   }
 
-  //get devices current location using location
-  // LocationData? currentLocation;
-  // void getCurrentLocation () {
-  //   Location location = Location();
-  //
-  //   location.getLocation().then(
-  //         (location) {
-  //           currentLocation = location;
-  //   },
-  //   );
-  // }
+
 
   Position? currentPosition;
   void getCurrentLocation() async {
