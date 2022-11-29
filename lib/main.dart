cfe4264db737b353ecc00cc604770b531fd33469
tmtkachyk <tmtkachyk@gmail.com>
2022-11-28 19:38:45 -0700

    draw hole in polygon calgary area

--- lib/main.dart
+++ lib/main.dart
@@ -47,7 +47,9 @@ class _MyAppState extends State<MyApp> {
           // given polygonId
           polygonId: PolygonId('1'),
           // initialize the list of points to display polygon
-          points: points,
+          points: entireMapPoints,
+          //draws a hole in the Polygon
+          holes: calgaryPoints,
           // given color to polygon
           //fillColor: Colors.green,
           fillColor: Colors.blueGrey.withOpacity(0.8),
@@ -57,7 +59,9 @@ class _MyAppState extends State<MyApp> {
           // given width of border
           strokeWidth: 4,
         )
+
     );
+
   }
 
   @override
@@ -108,7 +112,7 @@ class _MyAppState extends State<MyApp> {
   Set<Polygon> _polygon = HashSet<Polygon>();
 
 
-  List<LatLng> points = [
+  List<LatLng> entireMapPoints = [
 
    LatLng(85,90),  LatLng(85,0.1),
    LatLng(85,-90),  LatLng(85,-179.9),
@@ -116,13 +120,18 @@ class _MyAppState extends State<MyApp> {
    LatLng(-85,-90),  LatLng(-85,0.1),
   LatLng(-85,90),  LatLng(-85,179.9),
    LatLng(0,179.9), LatLng(85,179.9),
-
-
-
-
-
   ];
+
+  List<List<LatLng>>  calgaryPoints = [[
+
+    LatLng(51.183790624241, -114.2231309845041),  LatLng(51.183790624241, -113.86058216181912),
+    LatLng(50.85985482312287, -113.86058216181912),  LatLng(50.85985482312287, -114.2231309845041),
+
+  ]];
 }
+
+
+
 
 class MapPainter extends CustomPainter {
   final Map<String, MapLandmark> landmarksMap;
