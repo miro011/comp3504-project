67e7f135a9fe150eca0dfe660013c5a8f88647e5
mniko238 <mniko238@mtroyal.ca>
2022-12-06 18:47:33 -0700

    polygon generation while moving

--- lib/Globals.dart
+++ lib/Globals.dart
@@ -1,7 +1,9 @@
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:flutter/material.dart';
+import 'package:location/location.dart' as locations;
 
 const MAX_RECORDED_POSITIONS_IN_MEMORY = 10000;
+const LIGHT_DISTANCE = 0.0005;
 
 List<LatLng> ENTIRE_MAP_POINTS = [
   LatLng(85, 90), LatLng(85, 0.1),
@@ -22,7 +24,7 @@ List<List<LatLng>> CALGARY_POINTS = [[
 Polygon MAIN_POLYGON = Polygon(
   polygonId: PolygonId('1'),
   points: ENTIRE_MAP_POINTS, // list of points to display polygon
-  holes: CALGARY_POINTS, // draws a hole in the Polygon
+  //holes: CALGARY_POINTS, // draws a hole in the Polygon
   fillColor: Colors.blueGrey.withOpacity(0.8),
   strokeColor: Colors.blueGrey, // border color to polygon
   strokeWidth: 4, // width of border
--- lib/MyAppState.dart
+++ lib/MyAppState.dart
@@ -1,4 +1,5 @@
 import 'dart:collection';
+import 'dart:ffi';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
@@ -19,6 +20,7 @@ class MyAppState extends State<MyApp> {
   Position? CURRENT_POSITION;
   var RECORDED_POSITIONS = Queue<Tuple2<double, double>>();
   Set<Polygon> _POLYGONS_SET = HashSet<Polygon>();
+  int POLYGON_ID_COUNTER = 2; // 1 being the main one
 
   //............................................................................
 
@@ -27,7 +29,7 @@ class MyAppState extends State<MyApp> {
   void initState() {
     super.initState();
     _initLocationService();
-    _POLYGONS_SET.add(globals.MAIN_POLYGON);
+    //_POLYGONS_SET.add(globals.MAIN_POLYGON);
   }
 
   // "Future" not needed as we will not await this function
@@ -58,17 +60,75 @@ class MyAppState extends State<MyApp> {
   }
 
   void _onLocationChangedHandler(locations.LocationData loc) {
-    // Regulate RECORDED_POSITIONS size
-    if (RECORDED_POSITIONS.length >= globals.MAX_RECORDED_POSITIONS_IN_MEMORY) {
-      RECORDED_POSITIONS.removeFirst();
+    if (loc.latitude == null || loc.longitude == null) return;
+    double lat = loc.latitude ?? 0.0;
+    double long = loc.longitude ?? 0.0;
+
+    // remove negative values
+    lat += 90;
+    long += 180;
+
+    double xMin = long - globals.LIGHT_DISTANCE;
+    double xMax = long + globals.LIGHT_DISTANCE;
+    double yMin = lat - globals.LIGHT_DISTANCE;
+    double yMax = lat + globals.LIGHT_DISTANCE;
+
+    print("********************************************");
+    print(xMin.toString() + " " + xMax.toString() + " " + yMin.toString() + " " + yMax.toString());
+    print("********************************************");
+
+    for (Polygon poly in _POLYGONS_SET) {
+      bool xMatch = false;
+      bool yMatch = false;
+
+      double targetXMin = poly.holes[0][1].longitude + 180;
+      double targetXMax = poly.holes[0][0].longitude + 180;
+      double targetYMin = poly.holes[0][2].latitude + 90;
+      double targetYMax = poly.holes[0][0].latitude + 90;
+
+      print("////////////////////////////////////////////");
+      print(targetXMin.toString() + " " + targetXMax.toString() + " " + targetYMin.toString() + " " + targetYMax.toString());
+      print("////////////////////////////////////////////");
+
+      for (double x in [xMin, xMax]) {
+        if (x >= targetXMin && x <= targetXMax) xMatch = true;
+      }
+      for (double y in [yMin, yMax]) {
+        if (y >= targetYMin && y <= targetYMax) yMatch = true;
+      }
+
+      if (xMatch == true && yMatch == true) {
+        print("............................................");
+        print("exists");
+        print("............................................");
+        return;
+      } // collision detected
     }
 
-    // Save location
-    if (loc.latitude != null && loc.longitude != null) {
-      RECORDED_POSITIONS.addLast(
-          Tuple2<double, double>(loc.latitude!, loc.longitude!));
-      print("Just recorded ${loc.latitude} ${loc.longitude} for a total of ${RECORDED_POSITIONS.length}");
-    }
+    addPolygon([[
+      LatLng(yMax-90, xMax-180),
+      LatLng(yMax-90, xMin-180),
+      LatLng(yMin-90, xMax-180),
+      LatLng(yMin-90, xMin-180),
+    ]]);
+  }
+
+  void addPolygon(List<List<LatLng>> holes) {
+    _POLYGONS_SET.add(
+        Polygon(
+          polygonId: PolygonId(POLYGON_ID_COUNTER.toString()),
+          points: globals.ENTIRE_MAP_POINTS, // list of points to display polygon
+          holes: holes, // draws a hole in the Polygon
+          fillColor: Colors.blueGrey.withOpacity(0.8),
+          strokeColor: Colors.blueGrey, // border color to polygon
+          strokeWidth: 4, // width of border
+          geodesic: true,
+        )
+    );
+
+    POLYGON_ID_COUNTER += 1;
+
+    setState((){}); // re-runs the build method
   }
 
   //............................................................................
