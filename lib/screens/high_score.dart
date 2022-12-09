44d27894db727585ffff1ca168e91a2be86057a1
tmtkachyk <tmtkachyk@gmail.com>
2022-12-09 16:31:38 -0700

    Ordered High scores

--- lib/screens/high_score.dart
+++ lib/screens/high_score.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:term_project/Globals.dart';
 import 'package:term_project/config/classes.dart';
-
+import 'dart:collection';
 import '../API.dart';
 
 // import '../API.dart';
@@ -24,101 +24,123 @@ class HighScore extends StatefulWidget {
 
 class _HighScoreState extends State<HighScore> {
   Map scoreCounts = Map();
+  List<Widget> obj = <Widget>[];
+  String leadersID ="";
+  String leadersScores ="";
+  SplayTreeMap sortedScores = SplayTreeMap();
+
+
+
+
 
 
   @override
   void initState() {
     API.getHighscores().then((res) {
       setState(() {
+
         res.forEach((element) {
           if (!scoreCounts.containsKey(element)) {
             scoreCounts[element] = 1;
           } else {
             scoreCounts[element] += 1;
           }
+
+          sortedScores = SplayTreeMap.from(
+              scoreCounts, (key2, key1) => scoreCounts[key1].compareTo(scoreCounts[key2]));
+
         });
       });
 
-      print(scoreCounts.length);
-
-      scoreCounts.forEach((key, value) {
-        String leadersID = key;
-        int leadersScores = value;
-        // print('Recieved $leadersID + $leadersScores');
-      });
 
     });
+
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        flexibleSpace: Container(
-          decoration: const BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage('assets/images/background/bg05.png'),
-              fit: BoxFit.cover,
+
+    sortedScores.forEach((key, value) {
+      leadersID = key;
+      leadersScores = value.toString();
+      obj.add(
+          ListTile(
+            leading: Text(leadersID),
+            trailing: Text(
+              leadersScores,
+              style: TextStyle(color: Colors.green, fontSize: 15),
             ),
-          ),
-        ),
-        backgroundColor: Colors.transparent,
-        title: const Text('High Score'),
-      ),
-      drawer: Drawer(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            const HeaderDrawer(),
-            Expanded(
-              child: ListView(
-                padding: EdgeInsets.zero,
-                children: [
-                  //Home
-                  DrawerTile(
-                    index: 0,
-                    clickState: indexClicked,
-                  ),
-                  //High Score
-                  DrawerTile(
-                    index: 1,
-                    clickState: indexClicked,
-                  ),
-                  //Map Screen
-                  DrawerTile(
-                    index: 2,
-                    clickState: indexClicked,
-                  ),
-                  //Settings
-                  DrawerTile(
-                    index: 3,
-                    clickState: indexClicked,
-                  ),
-                  //About
-                  DrawerTile(
-                    index: 5,
-                    clickState: indexClicked,
-                  ),
-                  //Exit
-                  DrawerTile(
-                    index: 4,
-                    clickState: indexClicked,
-                  ),
-                ],
+          ));
+
+    });
+
+
+    return Scaffold(
+        appBar: AppBar(
+          flexibleSpace: Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/images/background/bg05.png'),
+                fit: BoxFit.cover,
               ),
             ),
-          ],
+          ),
+          backgroundColor: Colors.transparent,
+          title: const Text('High Score'),
         ),
-      ),
-      body: ListView.builder(
-        itemCount: scoreCounts.length,
-        itemBuilder: (BuildContext context, int index) {
-          return ListTile(
-            leading: const Text("test"),
-            trailing: const Text ("test"),
-          );
-        },
-      )
+        drawer: Drawer(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              const HeaderDrawer(),
+              Expanded(
+                child: ListView(
+                  padding: EdgeInsets.zero,
+                  children: [
+                    //Home
+                    DrawerTile(
+                      index: 0,
+                      clickState: indexClicked,
+                    ),
+                    //High Score
+                    DrawerTile(
+                      index: 1,
+                      clickState: indexClicked,
+                    ),
+                    //Map Screen
+                    DrawerTile(
+                      index: 2,
+                      clickState: indexClicked,
+                    ),
+                    //Settings
+                    DrawerTile(
+                      index: 3,
+                      clickState: indexClicked,
+                    ),
+                    //About
+                    DrawerTile(
+                      index: 5,
+                      clickState: indexClicked,
+                    ),
+                    //Exit
+                    DrawerTile(
+                      index: 4,
+                      clickState: indexClicked,
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+        body: ListView.builder(
+          itemCount: sortedScores.length,
+          itemBuilder: (BuildContext context, int index) {
+            return obj[index];
+          }),
     );
   }
+
+
+
 }
